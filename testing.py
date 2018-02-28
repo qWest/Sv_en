ff2dbe5ee54b6655301527dbb44a2befe8f18291
qWest <vladimir.gainullin@gmail.com>
2018-02-28 17:08:13 +0300

    testpy

---
+++ testing.py
@@ -0,0 +1,56 @@
+import telnetlib
+import time
+
+def sw_login(sw_telnet):
+    sw_telnet.write(b'admin\n')
+    sw_telnet.write(b'******\n')
+    time.sleep(1)
+    sw_output = sw_telnet.read_very_eager().decode('utf-8').split('\n')
+    sw_inv = sw_output[len(sw_output) - 1]
+    return(sw_inv)
+
+def sw_model(sw_telnet,sw_inv):
+    sw_telnet.write(b'sh sw\n')
+    sw_output = sw_telnet.read_until(bytes(sw_inv,"utf8")).decode('utf-8').split('\n')
+    print(sw_output)
+
+def sw_conf(sw_telnet,sw_inv):
+    sw_telnet.write(b'conf t\n')
+    time.sleep(1)
+    sw_telnet.write(b'passwords aging 0\n')
+    sw_telnet.write(b'sntp server 192.168.125.58 poll\n')
+    time.sleep(1)
+    sw_telnet.write(b'exit\n')
+    sw_telnet.write(b'write\n')
+    time.sleep(3)
+    sw_output = sw_telnet.read_very_eager().decode('utf-8')
+    print(sw_output)
+    sw_telnet.write(b'y\n')
+    sw_output = sw_telnet.read_until(bytes(sw_inv, "utf8")).decode('utf-8')
+    print(sw_output)
+
+ip_net = '172.16.23.'
+
+for i in range(254):
+    try:
+        telnet = telnetlib.Telnet(ip_net+str(i+1),23,5)
+        inv = sw_login(telnet)
+    except:
+        print('Error'+str(i+1))
+        continue
+    sw_conf(telnet,inv)
+    print(inv+' + '+str(i+1))
+    telnet.close()
+#telnet = telnetlib.Telnet(ip)
+#inv = sw_login(telnet)
+#print(inv)
+#sw_model(telnet,inv)
+#telnet.write(b'conf t\n')
+#telnet.write(b'hostname 1124_Gidgrostr_26_2#1\n')
+#telnet.write(b'show mac add\n')
+#time.sleep(0.5)
+#telnet.write(b'a')
+#time.sleep(2)
+#all_result = telnet.read_until(bytes(inv,"utf8")).decode('utf-8')
+#print(all_result)
+#telnet.close()
